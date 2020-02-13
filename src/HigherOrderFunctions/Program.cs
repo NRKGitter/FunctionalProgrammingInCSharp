b8d1cf3b3c0f844405220b108f5690d700d4ff11
Naresh Kripalani <nrkrips@gmail.com>
2020-02-12 19:36:53 -0500

    add initial procedural code

---
+++ src/HigherOrderFunctions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HigherOrderFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(args[0]);
+            var result = true;
+            for (int i = 2; i < number; i++)
+            {
+                if ( number % i == 0)
+                {
+                    result = false;
+                    break;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
